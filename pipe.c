aa8270a750138e272878e781d28d63b29e984963
atormac <atorma@proton.me>
2024-05-20 21:55:32 +0300

    Handle pipe() failure

--- pipe.c
+++ pipe.c
@@ -31,7 +31,15 @@ int *pipes_init(int argc)
 	{
 		if (pipe(pipes + (i * 2)) < 0)
 		{
-			ft_putstr_fd("pipe() failed\n", 1);
+			i--;
+			while (i >= 0)
+			{
+				close(pipes[i * 2]);
+				close(pipes[i * 2 + 1]);
+				i--;
+			}
+			free(pipes);
+			return (NULL);
 		}
 		i++;
 	}
@@ -42,12 +50,13 @@ void pipes_close(t_pipex_s *px, int *pipes)
 {
 	int cmd_count = px->argc - 3;
 	int i = 0;
+	int	*pipefd;
 
 	if (!pipes)
 		return;
 	while (i < (cmd_count - 1))
 	{
-		int *pipefd = pipes + (i * 2);
+		pipefd = pipes + (i * 2);
 		close(pipefd[0]);
 		close(pipefd[1]);
 		i++;
