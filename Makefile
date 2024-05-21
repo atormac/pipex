7b9975fb135e767a6e9dba44745264aefcf59404
Aarni Torma <atorma@student.hive.fi>
2024-05-21 16:28:12 +0300

    bonus files + makefile

--- Makefile
+++ Makefile
@@ -6,20 +6,22 @@
 #    By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/05 17:55:45 by atorma            #+#    #+#              #
-#    Updated: 2024/05/18 18:38:03 by atorma           ###   ########.fr        #
+#    Updated: 2024/05/21 16:23:16 by atorma           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = pipex
 CC = cc
 CFLAGS = -Wall -Wextra -Werror -g
+MANDATORYDIR = mandatory
+BONUSDIR = bonus
 SOURCES = main.c pipex.c pipe.c path.c utils.c exit_error.c
-BUILDDIR = mandatory
-OBJECTS = $(addprefix $(BUILDDIR)/,$(SOURCES:.c=.o))
+B_SOURCES = main_bonus.c pipex_bonus.c pipe_bonus.c path_bonus.c \
+			utils_bonus.c exit_error_bonus.c
+OBJECTS = $(addprefix $(MANDATORYDIR)/,$(SOURCES:.c=.o))
+B_OBJECTS = $(addprefix $(BONUSDIR)/,$(B_SOURCES:.c=.o))
 LIBDIR = ./libft
 
-$(BUILDDIR)/%.o: %.c
-	$(CC) -I$(BUILDDIR) -c $(CFLAGS) $< -o $@
 
 all: $(NAME)
 
@@ -27,13 +29,27 @@ $(NAME): $(OBJECTS)
 	$(MAKE) -C $(LIBDIR)
 	$(CC) -o $@ $(CFLAGS) $^ ./libft/libft.a
 
+$(MANDATORYDIR)/%.o: %.c
+	$(CC) -I$(MANDATORYDIR) -c $(CFLAGS) $< -o $@
+
+$(BONUSDIR)/%.o: %.c
+	$(CC) -I$(BONUSDIR) -c $(CFLAGS) $< -o $@
+
+.bonus: $(B_OBJECTS)
+	$(MAKE) -C $(LIBDIR)
+	$(CC) -o $($NAME) $(CFLAGS) $^ ./libft/libft.a
+	touch .bonus
+
+bonus: .bonus
+
 clean:
 	$(MAKE) -C $(LIBDIR) $@
 	rm -f $(OBJECTS)
 
 fclean: clean
 	rm -f $(NAME)
+	rm -f .bonus
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all bonus clean fclean re
---
+++ bonus/pipex_bonus.h
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 14:24:37 by atorma            #+#    #+#             */
+/*   Updated: 2024/05/18 17:47:05 by atorma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+
+enum
+{
+	PX_ERR_FILE,
+	PX_ERR_PERMS,
+	PX_ERR_CMD,
+	PX_ERR_FORK,
+	PX_ERR_DUP2,
+	PX_ERR_MALLOC,
+	PX_ERR_PATH,
+	PX_ERR_PIDS,
+	PX_ERR_PIPES
+};
+
+typedef struct t_pipex_s
+{
+	int		file1;
+	int		file2;
+	int		argc;
+	char	**argv;
+	char	**envp;
+	char	**path;
+	int		*pipes;
+	int		*pids;
+}	t_pipex_s;
+
+void	pipex_init(t_pipex_s *px, int argc, char **argv, char **envp);
+int		pipex_main(t_pipex_s *px);
+void	pipex_free_close(t_pipex_s *px);
+int		*pid_init(int argc);
+int		pid_wait(pid_t pid);
+int		*pipes_init(int argc);
+void	pipes_close(t_pipex_s *px, int *pipes);
+char	**path_get(char **envp);
+char	*path_join(char *path, char *bin);
+int		path_exec(char *cmd, t_pipex_s *px);
+void	free_array(char **arr);
+void	error_output(int error, char *str);
+void	exit_error(t_pipex_s *px, int error, char *str, int code);
+void	exit_silent(t_pipex_s *px, int code);
+
+#endif
