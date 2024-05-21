868f3e21dd4d9162330c4319d6749da0259a2b62
Aarni Torma <atorma@student.hive.fi>
2024-05-21 17:01:24 +0300

    heredoc

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -1,4 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 17:00:36 by atorma            #+#    #+#             */
+/*   Updated: 2024/05/21 17:01:15 by atorma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex_bonus.h"
+int	here_doc()
+{
+	while (1)
+	{
+
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -10,9 +29,14 @@ int	main(int argc, char **argv, char **envp)
 		ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", 1);
 		return (EXIT_SUCCESS);
 	}
-	if (ft_strncmp(argv[1], "heredoc", sizeof("heredoc") - 1) == 0)
+	if (ft_strncmp(argv[1], "here_doc", sizeof("here_doc") - 1) == 0)
 	{
 		ft_putstr_fd("heredoc1", 1);
+		int fd_here = open(STDIN_FILENO, O_RDONLY);
+		if (fd_here == -1)
+		{
+			ft_putstr_fd("could not open stdin!\n", 1);
+		}
 		return (EXIT_SUCCESS);
 	}
 	code = EXIT_FAILURE;
